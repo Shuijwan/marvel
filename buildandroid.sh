6e9222459436751b55024857f39699e04e6c0fcb
Shuijun wang <flyalizee@hotmail.com>
2016-06-26 14:57:00 +0800

    build script

---
+++ buildandroid.sh
@@ -0,0 +1,1 @@
+react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
