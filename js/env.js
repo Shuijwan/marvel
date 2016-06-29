6e41b848e85e032b8e8775560d2c4697695f179c
Shuijun wang <flyalizee@hotmail.com>
2016-06-29 20:33:19 +0800

    check platform

--- js/env.js
+++ js/env.js
@@ -5,7 +5,7 @@
 'use strict';
 var Platform = require('Platform');
 
-var serverUrl = Platform.OS === 'Android' ? 'http://gateway.marvel.com:80/v1/public/' : 'https://gateway.marvel.com:443/v1/public/';
+var serverUrl = Platform.OS === 'android' ? 'http://gateway.marvel.com:80/v1/public/' : 'https://gateway.marvel.com:443/v1/public/';
 
 module.exports = {
   PublicKey: 'd128928f89eab7dca3a7f3c003c48f80',
